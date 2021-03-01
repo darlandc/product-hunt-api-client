9dac2cbca754579a2cb73fbab14d95fc41380c47
Darlan <darlan.iribarrem@br.experian.com>
2021-03-01 13:32:46 -0300

    posts ok

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,24 +1,41 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 
 const Card = styled.span`
-	background: transparent;
-	border-radius: 3px;
-	border: 1px solid #EEE;
-	color: Gray;
-	margin: 5px 1em;
-	padding: 0.25em 1em;
-	border-radius: 6px;
-	box-shadow: 2px 2px 5px #C0C0C0;
+  background: transparent;
+  border-radius: 3px;
+  border: 1px solid #eee;
+  color: Gray;
+  margin: 1rem 1em;
+  padding: 0px 1em 1em 1em;
+  border-radius: 6px;
+  box-shadow: 2px 2px 5px #c0c0c0;
+`;
+
+const Thumbnail = styled.img`
+  border: 1px sold #eee;
+  width: 90%;
+  margin: 10px 0;
 `;
 
 const Post = (props) => {
+  const postInfo = props.postInfo;
+  console.log(postInfo);
 
-	return (
-		<>
-		<Card className="target">
-			<h1> {props.postInfo.name} x </h1>
-		</Card>
-		</>
-	);
+  return (
+    <>
+      <Card className="target">
+        <h4>{postInfo.name}</h4>
+        <p> {postInfo.tagline} </p>
+        <Thumbnail src={postInfo.thumbnail.url} />
+        <Button
+          variant="contained"
+          onClick={(event) => (window.location.href = `${postInfo.url}`)}
+        >
+          Open
+        </Button>
+      </Card>
+    </>
+  );
 };
 export default Post;
